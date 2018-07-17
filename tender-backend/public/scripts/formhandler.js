e4a4c5cf1f30f55fb3dad15195b70eda86673368
Blake Molina <blakeamolina@gmail.com>
2018-07-17 16:33:45 -0700

    adjust some comments

--- tender-backend/public/scripts/formhandler.js
+++ tender-backend/public/scripts/formhandler.js
@@ -4,9 +4,9 @@
   var $ = window.jQuery;
 
   /*
-   * The Constructor that sets the selector.
+   * The Constructor that sets the form selector.
    *
-   * @param selector The selector to select HTML elements and perform actions on them.
+   * @param selector The form selector to select HTML elements and perform actions on them.
    */
   function FormHandler(selector) {
     if (!selector) {
@@ -19,21 +19,19 @@
   }
 
   /*
-   * This method adds a submit handler for a form.
+   * Adds a submit handler for the form.
    *
-   * @param fn The function to apply on the form data after it gets submitted.
+   * @param fn The function to apply on the form data after it gets submitted by the user.
    */
   FormHandler.prototype.addSubmitHandler = function (fn) {
     console.log("Setting submit handler for form");
     this.$formElement.on("submit", function (event) {
       event.preventDefault();
-
       var data = {};
       $(this).serializeArray().forEach(function (item) {
         data[item.name] = item.value;
         console.log(item.name + " is " + item.value);
       });
-
       console.log(data);
       fn(data);
     });
@@ -44,20 +42,17 @@
    * each time the user modifies the Email field within the form, they should be notified
    * if the email they just entered is a valid email address.
    *
-   * TODO: Need to make this more generic to accept any selector.
-   *
-   * @param fn The function to apply on the input data as soon as it gets modified.
+   * @param fn The validation method to apply on the text input by the user.
    */
   FormHandler.prototype.addInputHandler = function(fn){
     console.log("Setting input handler for form");
-    // Add event handler for when user inputs data into the Email field
     this.$formElement.on("input", "[name=\"emailAddress\"]", function (event){
       var emailAddress = event.target.value;
       var message = "";
       if (fn(emailAddress)){
         event.target.setCustomValidity("");
       } else{
-        message = emailAddress + " is not an authorized email address!";
+        message = emailAddress + " is not a valid email address!";
         event.target.setCustomValidity(message);
       }
     });

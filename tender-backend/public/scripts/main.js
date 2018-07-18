eb5f533c0cbff5dc38db27b0b41d92bb4f29a94f
ibcurly <curtslayer@gmail.com>
2018-07-17 23:16:35 -0700

    commented out calls to the skeleton row js files foods comments and friends

--- tender-backend/public/scripts/main.js
+++ tender-backend/public/scripts/main.js
@@ -1,21 +1,22 @@
 (function (window) {
   "use strict";
   var $ = window.jQuery;
-  var App = window.App;
 
-  var COMMENT_LIST_SELECTOR = '[test="commentlist"]'; // TODO fix according to html
-  var CommentList = App.CommentList;
-  var commentList = new CommentList(COMMENT_LIST_SELECTOR);
+  // var App = window.App;
+
+  // var COMMENT_LIST_SELECTOR = '[test="commentlist"]'; // TODO fix according to html
+  // var CommentList = App.CommentList;
+  // var commentList = new CommentList(COMMENT_LIST_SELECTOR);
   //commentList.addRow.call(commentList, data);
 
-  var FRIENDS_LIST_SELECTOR = '[test="friendslist"]'; // TODO fix according to html
-  var FriendsList = App.FriendsList;
-  var friendsList = new FriendsList(FRIENDS_LIST_SELECTOR);
+  // var FRIENDS_LIST_SELECTOR = '[test="friendslist"]'; // TODO fix according to html
+  // var FriendsList = App.FriendsList;
+  // var friendsList = new FriendsList(FRIENDS_LIST_SELECTOR);
   //friendsList.addRow.call(friendsList, data);
 
-  var FOOD_LIST_SELECTOR = '[test="foodlist"]'; // TODO fix according to html
-  var FoodList = App.FoodList;
-  var foodList = new FoodList(FOOD_LIST_SELECTOR);
+  // var FOOD_LIST_SELECTOR = '[test="foodlist"]'; // TODO fix according to html
+  // var FoodList = App.FoodList;
+  // var foodList = new FoodList(FOOD_LIST_SELECTOR);
   //foodList.addRow.call(foodList, data);
 
 

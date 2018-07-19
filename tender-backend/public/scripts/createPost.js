32667ac334de27eae84b9de8faad2b54fd5b6c6c
Andrew De La Fuente <delafuenteandrew@ymail.com>
2018-07-19 15:07:03 -0700

    Improved funtionality of modal pop-up, and now database stores message with picture

--- tender-backend/public/scripts/createPost.js
+++ tender-backend/public/scripts/createPost.js
@@ -1,14 +1,18 @@
 function convertToBase64(callback) {
-  console.log('buttonPressed');
+  console.log("Trying to save picture to database");
 
+  var $ = window.jQuery;
   var image = document.querySelector("input[type=file]").files[0];
+  // text for the photo posted
+  var text = document.getElementById('foodMessage').value;
+  console.log(text);
   var reader = new FileReader();
   console.log(image);
   if (image) {
     reader.readAsDataURL(image);
     reader.onload = function() {
       $.post('http://localhost:2403/pictures', {
-        'file': reader.result
+        'file': reader.result, 'message': text
       }, function(result) {
         $.get("http://localhost:2403/pictures", function(result) {
           console.log(result);
@@ -16,9 +20,24 @@ function convertToBase64(callback) {
         });
       });
     };
+    // close the modal box on submitted
+    document.getElementById("myModal").modal("hide");
+    $("#submitError").hide();
 
     reader.onerror = function(error) {
       console.log('Error: ', error);
     };
   }
+  else{
+    $("#submitError").show();
+  }
+}
+
+function createPost(callback)
+{
+  console.log("Creating a Post");
+  // Used if picture has been uploaded by user
+  convertToBase64();
+  // Used if user has typed a message initial
+
 }
